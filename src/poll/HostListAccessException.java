b702f72306a4de9a076d3a9da8fc31dd81dc9e2d
hanpei <happysoftware@foxmail.com>
2014-12-02 17:36:58 +0800

    增加  在未完成主机信息采集情况下，读取某个主机信息时出现访问异常

---
+++ src/poll/HostListAccessException.java
@@ -0,0 +1,19 @@
+package poll;
+
+public class HostListAccessException extends Exception {
+
+	
+	public HostListAccessException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
