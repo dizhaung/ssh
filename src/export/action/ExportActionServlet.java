a7faf0199340806e0ab282241e2523a917ef5995
hanpei <happysoftware@foxmail.com>
2014-12-27 00:38:42 +0800

    修改  采集之后并且有被采集的服务器方能导出文件

--- src/export/action/ExportActionServlet.java
+++ src/export/action/ExportActionServlet.java
@@ -18,6 +18,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
  
 import export.io.File;
 import export.io.POIReadAndWriteTool;
@@ -29,7 +32,7 @@ import net.sf.json.JSONArray;
 
 public class ExportActionServlet extends HttpServlet {
 
-
+	private static Log logger = LogFactory.getLog(ExportActionServlet.class); 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
@@ -43,18 +46,29 @@ public class ExportActionServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		//设置返回内容的编码格式
 		resp.setCharacterEncoding("utf-8"); 
-		String userDir = req.getRealPath("/");
-		 System.out.println(userDir);
-		 Date currentTime    = new Date();
-		 SimpleDateFormat timeFormater = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
-		 String time = timeFormater.format(currentTime);
-	        String fileName = time;  
-	        String fileType = "xlsx";   
-	        POIReadAndWriteTool writer = POIReadAndWriteTool.getInstance();
-	        writer.write((List<Host>)req.getSession().getServletContext().getAttribute("host"),new File(userDir+"export"+java.io.File.separator, fileName, fileType));
-	        
-	        PrintWriter out = resp.getWriter();
-	       out.print("/ssh/export/"+fileName+"."+fileType);
+		 List<Host> hostList =  (List<Host>)getServletContext().getAttribute("hostlist");
+		 PrintWriter out = resp.getWriter();
+
+		// 采集之后方能导出文件  并且有被采集的服务器
+		if (hostList != null && hostList.size() > 0) {
+			String userDir = req.getRealPath("/");
+			System.out.println(userDir);
+			Date currentTime = new Date();
+			SimpleDateFormat timeFormater = new SimpleDateFormat(
+					"yyyy-MM-dd-HH-mm-ss");
+			String time = timeFormater.format(currentTime);
+			String fileName = time;
+			String fileType = "xlsx";
+			logger.info("-----将服务器数据写入文件-----");
+			POIReadAndWriteTool writer = POIReadAndWriteTool.getInstance();
+			
+			writer.write(hostList, new File(userDir + "export"
+					+ java.io.File.separator, fileName, fileType));
+			
+			logger.info("导出文件的路径=/ssh/export/" + fileName + "." + fileType);
+			out.print("/ssh/export/" + fileName + "." + fileType);
+			out.flush();
+		}
 	}
 
 	/**
