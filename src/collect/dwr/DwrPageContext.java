60d2cccc46a2efd26774030edb1617c18b8c7ce5
hanpei <happysoftware@foxmail.com>
2015-01-02 16:06:57 +0800

    修改  在非DWRThread下，通过Browser api，实现Reverse Ajax

--- src/collect/dwr/DwrPageContext.java
+++ src/collect/dwr/DwrPageContext.java
@@ -4,50 +4,35 @@ import java.util.Collection;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.directwebremoting.Browser;
 import org.directwebremoting.ScriptBuffer;
 import org.directwebremoting.ScriptSession;
-import org.directwebremoting.WebContext;
-import org.directwebremoting.WebContextFactory;
-import org.directwebremoting.proxy.dwr.Util;
+import org.directwebremoting.ScriptSessions;
+import org.directwebremoting.ServerContextFactory;
+
+
+
 
 public class DwrPageContext {
 
 	private static Log logger = LogFactory.getLog(DwrPageContext.class);
-	public static WebContext contex;
-	public static Collection<ScriptSession> sessions ;
-	public static  Util util;
-	public static String functionName;
-	/**
-	 * 
-	 * @param pagePath		要推送的页面页面
-	 * @param functionName		推送消息到页面上，处理消息的回调函数
-	 */
-    @SuppressWarnings("deprecation")  
-    public void init(String pagePath,String functionName){  
-        //得到上下文  
-        contex = WebContextFactory.get();
-        String rootPath = contex.getServletContext().getContextPath();
-       
-        //得到要推送到 的页面  dwr3为项目名称
-        sessions = contex.getScriptSessionsByPage(rootPath+pagePath);  
-        logger.info(rootPath+pagePath);
-        //
-        util = new Util(sessions);  
-        this.functionName = functionName;  
-        
-    }
-    private static int count = 0;
     public static void run(String msg){
+    	String rootPath = ServerContextFactory.get().getContextPath();
     	//下面是创建一个javascript脚本 ， 相当于在页面脚本中添加了一句  functionName(msg);   
-        ScriptBuffer sb = new ScriptBuffer();  
-        sb.appendScript(functionName+"(");  
+    	final  ScriptBuffer sb = new ScriptBuffer();  
+        sb.appendScript("show(");  
         sb.appendData(msg);  
         sb.appendScript(")");  
       
-        //推送 
-        if(util != null){
-        	 util.addScript(sb);  
-        }
+        Browser.withPage(rootPath+"/index.html",new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				ScriptSessions.addScript(sb);
+			}
+        	
+        });
        
     }
 	/**
