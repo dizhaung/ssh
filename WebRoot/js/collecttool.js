5168bf155a1b527fb4174afab131690ad3efef5d
hanpei <happysoftware@foxmail.com>
2015-03-02 11:35:09 +0800

    测试

--- WebRoot/js/collecttool.js
+++ WebRoot/js/collecttool.js
@@ -16,11 +16,8 @@ var CollectTool = (function(){
 		exportFileFrom:function(url){
 			this.disableButtons(url.buttonIds);//禁用导出按钮
 			$.get(url.url,function(data){
-				 	console.log(data);
 				$('#exportIframe').attr('src',data);
-				
 			});
-		 
 			this.enableButtons(url.buttonIds);
 		},
 		'disableButtons':function(buttonIds){
