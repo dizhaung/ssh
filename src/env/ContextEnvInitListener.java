fa055540e68d494e0b30a72fd5c86be7a5de631c
hanpei <happysoftware@foxmail.com>
2015-06-25 18:44:18 +0800

    修正 设置日志路径等环境的监听加入版本管理

---
+++ src/env/ContextEnvInitListener.java
@@ -0,0 +1,34 @@
+package env;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * 初始化log4j日志文件输出根路径
+ * @author HP
+ *
+ */
+public class ContextEnvInitListener implements ServletContextListener {
+
+    @Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		System.getProperties().remove("log4jdir");
+	}
+
+    
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		String path = arg0.getServletContext().getRealPath("/");
+		System.setProperty("log4jdir", path);
+	}
+
+	/**
+     * Default constructor. 
+     */
+    public ContextEnvInitListener() {
+        // TODO Auto-generated constructor stub
+    }
+	
+}
